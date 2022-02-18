cae8625e0bcceb4f38a790b943222fff1e99193a
Yashsinha220 <sinhayash63@gmail.com>
2022-02-19 01:35:08 +0530

    linkedlist

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -20,44 +20,56 @@ public:
     }
 };
 
-void insertionatstart(node * &head, int data) // this will return the first point
+void insertionatstart(node *&head, int data) // this will return the first point
 {
     // node *temp= new node(data, head);
     // return temp;
-    //or
-    node * temp=new node(data);
-    temp->next=head;
-    head=temp;
-    
+    // or
+    node *temp = new node(data);
+    temp->next = head;
+    head = temp;
 }
-void insertionatlast(node * &head , int data)
+void insertionatlast(node *&head, int data)
 {
-    //creating the new pointer 
-    node * p= head;//for the track
-    node * temp=new node(data);//for the insertion
-    while (p->next !=NULL)
+    // creating the new pointer
+    node *p = head;              // for the track
+    node *temp = new node(data); // for the insertion
+    while (p->next != NULL)
     {
-        p=p->next;
+        p = p->next;
     }
-    p->next=temp;//pointiong the p to the temp 
-    temp->next=NULL;//pointing the temp to the null
+    p->next = temp;    // pointiong the p to the temp
+    temp->next = NULL; // pointing the temp to the null
 }
-void printlinkedlist(node * &head)//passing the refrence
+void printlinkedlist(node *&head) // passing the refrence
 {
-    
-    node * temp=head;
+
+    node *temp = head;
     while (temp != NULL)
     {
-        cout << temp->data<<" ";
+        cout << temp->data << " ";
         temp = temp->next;
     }
 }
 
-void insertmiddle(node * ptr , int data)
+void insertmiddle(node *ptr, int data)
 {
-    node * temp=new node(data);
-    temp->next=ptr->next;//first pointing the next of the temp to the ptr next
-    ptr->next=temp;//pointing the ptr to the temp
+    node *temp = new node(data);
+    temp->next = ptr->next; // first pointing the next of the temp to the ptr next
+    ptr->next = temp;       // pointing the ptr to the temp
+}
+void insertatposition(int position, int data, node *head)
+{
+    node *p = head;
+    int count = 1;
+    while (count < position - 1)
+    {
+        p = p->next;
+        count++;
+    }
+    node *nodetoinsert = new node(data);
+    nodetoinsert->next = p->next;
+    p->next = nodetoinsert;
 }
 int main()
 {
@@ -65,17 +77,22 @@ int main()
     node *second = new node(12);
     head->next = second;
     second->next = nullptr;
-    
+
     printlinkedlist(head);
-    cout<<endl;
- insertionatstart(head,11);
- insertionatstart(head,123);
+    cout << endl;
+    insertionatstart(head, 11);
+    insertionatstart(head, 123);
     printlinkedlist(head);
-    cout<<endl;
-     insertionatlast(head,780);
-      insertionatlast(head,1238);
+    cout << endl;
+    insertionatlast(head, 780);
+    insertionatlast(head, 1238);
     printlinkedlist(head);
-    
+    cout << endl;
+    insertmiddle(second, 678);
+    printlinkedlist(head);
+    cout << endl;
+    insertatposition(3, 900, head);
+    printlinkedlist(head);
 
     return 0;
 }
